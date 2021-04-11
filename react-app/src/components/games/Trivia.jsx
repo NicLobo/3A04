1bfe02f38869533d31f43aa65dfc75bc9feedc0b
Nicholas Lobo <69403302+NicLobo@users.noreply.github.com>
2021-04-10 22:29:06 -0400

    uploaded trivia

---
+++ react-app/src/components/games/Trivia.jsx
@@ -0,0 +1,137 @@
+import React from "react";
+import Sketch from 'react-p5'
+
+import QA from "./trivia-classes/QA";
+import Healthbar from "./trivia-classes/Healthbar";
+import Timebar from "./trivia-classes/Timebar";
+import Button from "./trivia-classes/Button"
+
+function Trivia({back, difficulty, character}) {
+  const UNIT = 10; 
+  let width = 1000;
+  let height = 700;
+  let endgame = "playing";
+  let questions
+  let health
+  let time
+  let buttonA;
+  let buttonB;
+  let buttonC; 
+  let buttonD;
+  let i = 0;
+  let flag = 1; 
+  const setup = (p, canvasParentRef) => {
+
+    //background
+    p.createCanvas(width, height).parent(canvasParentRef);
+    p.textFont('Space Mono'); 
+    p.smooth(); 
+
+    //font
+    p.fill(0);
+    p.textSize(30);
+    p.textFont('Space Mono');
+
+    //objects
+    questions = new QA(difficulty, p);
+
+    buttonA = new Button(p, width/1.5, height/2, UNIT*10, UNIT*10, 'rgb(0,255,0)',"A");
+    buttonB = new Button(p, width/1.5 + 150, height/2, UNIT*10, UNIT*10, 'rgb(255,0,0)',"B");
+    buttonC = new Button(p, width/1.5, height/2 +150, UNIT*10, UNIT*10, 'rgb(0,0,255)',"C");
+    buttonD = new Button(p, width/1.5 + 150, height/2 +150, UNIT*10, UNIT*10,'rgb(0,255,255)',"D");
+
+    health = new Healthbar(3, UNIT, UNIT*0, UNIT*5, p, difficulty);
+    time = new Timebar(UNIT*90, UNIT, UNIT*0, UNIT*6, p);
+  }
+
+
+  let draw = (p) => {
+    
+    p.clear();
+    p.fill(0);
+    p.textSize(30);
+    p.text('Answer the security questions ' + character + "!", UNIT*0, UNIT*3);
+    time.display();
+    health.display();
+    questions.display(width,height);
+    buttonA.display();
+    buttonB.display();
+    buttonC.display();
+    buttonD.display();
+
+    if(endgame === "playing"){
+        time.tickTime();
+        if(time.isTimeout()) {
+          health.takeDamge();
+          questions.newQuestion();
+          time.reset();
+        }
+      
+    else if(health.isDead()){
+        endgame = "lost"
+      }
+    else if(questions.noQuestions()){
+        endgame = "won"
+      }
+    }
+    if(endgame === "won"){
+      p.textSize(100);
+      i = i+1;
+      if (i % 50 === 0) flag *=-1
+      if(flag == 1) p.fill(0,255,0);
+      if(flag == -1)p.fill(255,255,255);  
+
+      p.text("ACCESS GRANTED!", UNIT*10, UNIT*30);
+    }
+
+    if(endgame === "lost"){
+      p.textSize(100);
+      i = i+1;
+      if (i % 50 === 0)flag *=-1
+      if(flag == 1) p.fill(255,0,0);
+      if(flag == -1)p.fill(255,255,255); 
+    
+      p.text("ACCES DENIED!", UNIT*10, UNIT*30);
+    }
+    p.redraw();
+  }
+
+  function mousePressed(p) {
+   if(endgame === "playing")
+    if(buttonA.isHit(p.mouseX, p.mouseY)){
+      if(!questions.checkAnswer("A")) health.takeDamge();
+      questions.newQuestion();
+      time.reset();
+    }
+    else if(buttonB.isHit(p.mouseX, p.mouseY)){
+      if(!questions.checkAnswer("B")) health.takeDamge();
+      questions.newQuestion();
+      time.reset();
+    }
+    else if(buttonC.isHit(p.mouseX, p.mouseY)){
+      if(!questions.checkAnswer("C")) health.takeDamge();
+      questions.newQuestion();
+      time.reset();
+    }
+    else if(buttonD.isHit(p.mouseX, p.mouseY)){
+      if(!questions.checkAnswer("D")) health.takeDamge();
+      questions.newQuestion();
+      time.reset();
+    }
+  }
+
+
+
+  return (
+    <div className="text-center">
+        <h3>Trivia</h3>
+        <button onClick={back}>Back to game menu</button>
+        <div className="game mt-2">
+          <Sketch setup={setup} draw={draw} mousePressed={mousePressed}/>
+        </div>
+    </div>
+  );
+
+
+}
+export default Trivia;
