1d64040d46b7257f5a372f0aff02151696b94c02
Graeme Woods <graemewoods202@gmail.com>
2021-04-14 01:07:28 -0400

    tweak trivia wording

--- react-app/src/components/games/trivia-classes/QA.js
+++ react-app/src/components/games/trivia-classes/QA.js
@@ -5,7 +5,7 @@ class QA{
       this.numquestions = 5;
       this.questionList=[
       "Which of the following languages is used as a scripting language in the Unity 3D game engine?",
-      "What is the largest organ of the human body?",
+      "What is the largest organ in the human body?",
       "What geometric shape is generally used for stop signs? ",
       "What do the letters of the fast food chain KFC stand for? ",
       "What is the closest planet to our solar system's sun?",
@@ -19,7 +19,7 @@ class QA{
       this.optionList = [
         "A. Java B. C# C. C++ D. Objective-C",
         "A. Skin B. Heart C. Large Intestine D. Liver",
-        "A. Triangle B. Circle C. Hexagon D. Triangle",
+        "A. Triangle B. Circle C. Octagon D. Triangle",
         "A. Kiwi Food Cut B. Kibbled Freaky Cow C. Kentucky Fried Chicken D. Kentucky Fresh Cheese",
         "A. Mars B. Mercury C. Jupiter D. Earth",
         "A. Tiger B. Panda C. Gorilla D. Crocodile",
@@ -28,7 +28,7 @@ class QA{
         "A. The Milky Way Galaxy B. The Mars Galaxy C. The Galaxy Note D. The Black Hole",
         "A. Marie Curie B. Louis Pasteur C. Alfred Nobel D. Alexander Fleming"
     ]
-      this.answerList = ["C", "A", "C","C","B","C","D","A","A","D"]
+      this.answerList = ["B", "A", "C","C","B","C","D","A","A","D"]
       this.questionLeft= [0,1,2,3,4,5,6,7,8,9]
 
 
